d790939ba08c970f517e02230b2055722dba1a48
Cadel Watson <cadel@cadelwatson.com>
2019-01-05 14:15:31 +1100

    Add ingredient import into database

---
+++ cmd/importIngredients.go
@@ -0,0 +1,160 @@
+package cmd
+
+import (
+	"fmt"
+
+	"bufio"
+	"encoding/csv"
+	"io"
+	"os"
+
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+	"github.com/kdelwat/recipaliser/db"
+	"github.com/kdelwat/recipaliser/model"
+	"github.com/spf13/cobra"
+	"gopkg.in/cheggaaa/pb.v1"
+)
+
+var importIngredientsCommand = &cobra.Command{
+	Use:   "import",
+	Short: "Import ingredients into the database",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		importIngredients(args[0])
+	},
+}
+
+func init() {
+	ingredientCmd.AddCommand(importIngredientsCommand)
+}
+
+func importIngredients(filename string) {
+	csvFile, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Printf("Could not import %v: %v", filename, err)
+		os.Exit(1)
+	}
+
+	csvReader := csv.NewReader(bufio.NewReader(csvFile))
+
+	_, err = csvReader.Read()
+
+	if err != nil {
+		fmt.Printf("Could not read header line of %v: %v", filename, err)
+	}
+
+	var ingredients []model.Ingredient
+
+	lineNo := 1
+	for {
+		line, err := csvReader.Read()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Printf("Could not import %v at line %v: %v", filename, lineNo, err)
+			os.Exit(1)
+		}
+
+		var nutritionalData []float64
+		for _, nutritionalInfo := range line[4:] {
+			nutritionalInfoAsFloat, err := strconv.ParseFloat(nutritionalInfo, 64)
+
+			if err != nil {
+				fmt.Printf("Could not import %v at line %v: could not convert %v to float", filename, lineNo, nutritionalInfo)
+				os.Exit(1)
+
+			}
+
+			nutritionalData = append(nutritionalData, nutritionalInfoAsFloat)
+		}
+
+		ingredients = append(ingredients, model.Ingredient{
+			AusnutID:                  line[0],
+			EnergyWithDietaryFibre:    nutritionalData[0],
+			EnergyWithoutDietaryFibre: nutritionalData[1],
+			Moisture:                  nutritionalData[2],
+			Protein:                   nutritionalData[3],
+			TotalFat:                  nutritionalData[4],
+			AvailableCarbohydratesWithSugarAlcohols:   nutritionalData[5],
+			AvailableCarbohydratesWithoutSugarAlcohol: nutritionalData[6],
+			Starch:       nutritionalData[7],
+			TotalSugars:  nutritionalData[8],
+			AddedSugars:  nutritionalData[9],
+			FreeSugars:   nutritionalData[10],
+			DietaryFibre: nutritionalData[11],
+			Alcohol:      nutritionalData[12],
+			Ash:          nutritionalData[13],
+			PreformedVitaminARetinol:           nutritionalData[14],
+			BetaCarotene:                       nutritionalData[15],
+			ProvitaminABetaCaroteneEquivalents: nutritionalData[16],
+			VitaminARetinolEquivalents:         nutritionalData[17],
+			ThiaminB1:                          nutritionalData[18],
+			RiboflavinB2:                       nutritionalData[19],
+			NiacinB3:                           nutritionalData[20],
+			NiacinDerivedEquivalents:           nutritionalData[21],
+			FolateNatural:                      nutritionalData[22],
+			FolicAcid:                          nutritionalData[23],
+			TotalFolates:                       nutritionalData[24],
+			DietaryFolateEquivalents:           nutritionalData[25],
+			VitaminB6:                          nutritionalData[26],
+			VitaminB12:                         nutritionalData[27],
+			VitaminC:                           nutritionalData[28],
+			AlphaTocopherol:                    nutritionalData[29],
+			VitaminE:                           nutritionalData[30],
+			CalciumCa:                          nutritionalData[31],
+			IodineI:                            nutritionalData[32],
+			IronFe:                             nutritionalData[33],
+			MagnesiumMg:                        nutritionalData[34],
+			PhosphorusP:                        nutritionalData[35],
+			PotassiumK:                         nutritionalData[36],
+			SeleniumSe:                         nutritionalData[37],
+			SodiumNa:                           nutritionalData[38],
+			ZincZn:                             nutritionalData[39],
+			Caffeine:                           nutritionalData[40],
+			Cholesterol:                        nutritionalData[41],
+			Tryptophan:                         nutritionalData[42],
+			TotalSaturatedFat:                  nutritionalData[43],
+			TotalMonounsaturatedFat:            nutritionalData[44],
+			TotalPolyunsaturatedFat:            nutritionalData[45],
+			LinoleicAcid:                       nutritionalData[46],
+			AlphalinolenicAcid:                 nutritionalData[47],
+			C205w3Eicosapentaenoic:             nutritionalData[48],
+			C225w3Docosapentaenoic:             nutritionalData[49],
+			C226w3Docosahexaenoic:              nutritionalData[50],
+			TotalLongChainOmega3FattyAcids:     nutritionalData[51],
+			TotalTransFattyAcids:               nutritionalData[52]})
+
+		lineNo++
+	}
+
+	progressBar := pb.StartNew(len(ingredients))
+
+	ingredientsImported := 0
+	ingredientsSkipped := 0
+	for _, ingredient := range ingredients {
+		var existingIngredient model.Ingredient
+
+		err := db.Db.First(existingIngredient, "ausnut_id = ?", ingredient.AusnutID).Error
+
+		if err == gorm.ErrRecordNotFound {
+			ingredientsImported++
+			db.Db.Create(&ingredient)
+		} else if err != nil {
+			fmt.Printf("Could not import %v: %v", filename, err)
+			os.Exit(1)
+		} else {
+			ingredientsSkipped++
+		}
+		progressBar.Increment()
+	}
+
+	progressBar.FinishPrint("Complete")
+
+	fmt.Println("Import completed successfully")
+	fmt.Printf("Imported %v ingredients\n", ingredientsImported)
+	fmt.Printf("Skipped %v ingredients that were already present in database\n", ingredientsSkipped)
+}
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/kdelwat/recipaliser/cmd"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/kdelwat/recipaliser/cmd"
+	"github.com/kdelwat/recipaliser/db"
+	"github.com/kdelwat/recipaliser/model"
+)
 
 func main() {
+	var err error
+	db.Db, err = gorm.Open("sqlite3", "./recipaliser.db")
+
+	if err != nil {
+		fmt.Println("Failed to create database")
+		fmt.Println(err)
+	}
+
+	defer db.Db.Close()
+
+	db.Db.AutoMigrate(&model.Ingredient{})
 	cmd.Execute()
 }
